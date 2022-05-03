0921fb36ab2a49d7652c03bae57251338d415dcf
minhtri06ltr <laptopdienthoai1@gmail.com>
2022-05-03 11:15:40 +0000

    setup with nextjs, tailwindcss, sanity

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>MEMORYZONE</title>
+        <meta name="description" content="MEMORYZONE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
