6ddb3b8894d0ef5fbe86f9f747e48af059257939
minhtri06ltr <79675694+minhtri06ltr@users.noreply.github.com>
2022-05-05 14:33:34 +0700

    Update ProductCard.jsx

--- client/components/ProductCard.jsx
+++ client/components/ProductCard.jsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 const ProductCard = () => {
   return (
     <div className="">
-      <div>
+      <div className="relative aspect-square">
         <Link href="">
           <a>
             <Image
               src="https://bizweb.sapocdn.net/thumb/medium/100/329/122/products/laptop-lenovo-yoga-slim-7-pro-14ihu5-o-82nh00aevn.png?v=1651221446000"
-              layout="responsive"
-              width="100%"
-              height={98}
+              layout="fill"
+              objectFit="contain"
+              quality={100}
             />
           </a>
         </Link>
       </div>
       <Link href="">
-        <a className="text-text hover:text-primary limit-3-line my-2">
+        <a className="text-text leading-none hover:text-primary limit-3-line my-2">
           Laptop Lenovo Yoga Slim 7 Pro 14IHU5 O 82NH00AEVN (i5-11300H EVO, Iris
           Xe Graphics, Ram 16GB DDR4, SSD 512GB, 14 Inch OLED 2.8K)
         </a>
