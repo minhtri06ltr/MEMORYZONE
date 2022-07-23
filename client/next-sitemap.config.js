0b808e3b1802204935e07ed693abbfca5e1e7656
minhtri06ltr <laptopdienthoai1@gmail.com>
2022-07-23 07:43:12 +0000

    duplicate sitemap

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 
-
-module.exports ={
-    siteUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
-    generateRobotsTxt: true,
-    robotsTxtOptions:{
-        additionalSitemaps:[`${process.env.NEXT_PUBLIC_CLIENT_URL}/server-sitemap.xml`,`${process.env.NEXT_PUBLIC_CLIENT_URL}/sitemap.xml`]
-    }
-}
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/server-sitemap.xml`,
+    ],
+  },
+};
--- client/pages/server-sitemap.xml/index.jsx
+++ client/pages/server-sitemap.xml/index.jsx
@@ -1,32 +1,23 @@
-
-
 import { getServerSideSitemap } from "next-sitemap";
-import {client} from '../../lib/client'
+import { client } from "../../lib/client";
 
-export const getServerSideProps  = async(context) =>{
+export const getServerSideProps = async (context) => {
+  const category = await client.fetch(`*[_type=='category']{categoryName}`);
 
-    const order = await client.fetch(`*[_type=='order']{_id}`);
-    const orderArray  = order.map((item)=>({
-        loc: `${process.env.NEXT_PUBLIC_CLIENT_URL}/checkout/${item.isPaid ? 'success/' : '' }${item._id}`,
-        lastmod: new Date().toISOString(),
-    }));
-    const category = await client.fetch(`*[_type=='category']{categoryName}`);
- 
-    const categoryArray = category.map((item)=>({
-        loc: `${process.env.NEXT_PUBLIC_CLIENT_URL}/category/${item.categoryName}`,
-        lastmod: new Date().toISOString(),
-    }));
-    const brand = await client.fetch(`*[_type=='brand']{productBrand}`);
-   
-    const brandArray = brand.map((item)=>({
-        loc: `${process.env.NEXT_PUBLIC_CLIENT_URL}/brand/${item.productBrand}`,
-        lastmod: new Date().toISOString(),
-    }));
-   
-    const fields = orderArray.concat(categoryArray,brandArray);
+  const categoryArray = category.map((item) => ({
+    loc: `${process.env.NEXT_PUBLIC_CLIENT_URL}/category/${item.categoryName}`,
+    lastmod: new Date().toISOString(),
+  }));
+  const brand = await client.fetch(`*[_type=='brand']{productBrand}`);
 
-return getServerSideSitemap(context,fields)
-}
+  const brandArray = brand.map((item) => ({
+    loc: `${process.env.NEXT_PUBLIC_CLIENT_URL}/brand/${item.productBrand}`,
+    lastmod: new Date().toISOString(),
+  }));
 
+  const fields = categoryArray.concat(brandArray);
 
-export default function Site  () {}
+  return getServerSideSitemap(context, fields);
+};
+
+export default function Site() {}
